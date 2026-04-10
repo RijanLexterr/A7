e39494845e83a05914943cfae52b2c0deee236f2
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2026-04-10 15:58:08 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -98,65 +98,65 @@
 
             <div class="row g-4" id="gallery-grid">
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/651901776_4209519825973609_6437340701431109741_n.jpg"><div class="caption">Bulldozer Unit 1</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/651901776_4209519825973609_6437340701431109741_n.jpg"><div class="caption">Bulldozer Unit 1</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/659029423_752292474487918_7039981739803273036_n.jpg"><div class="caption">Bulldozer Unit 2</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/659029423_752292474487918_7039981739803273036_n.jpg"><div class="caption">Bulldozer Unit 2</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/655274188_782402084602224_5329138535872013539_n.jpg"><div class="caption">Bulldozer Unit 3</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/655274188_782402084602224_5329138535872013539_n.jpg"><div class="caption">Bulldozer Unit 3</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/652739421_1448789470325343_3930862270811786554_n.jpg"><div class="caption">Bulldozer Unit 4</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/652739421_1448789470325343_3930862270811786554_n.jpg"><div class="caption">Bulldozer Unit 4</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/654365048_3443767425772096_454726511502435370_n.jpg"><div class="caption">Bulldozer Unit 5</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/654365048_3443767425772096_454726511502435370_n.jpg"><div class="caption">Bulldozer Unit 5</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/655171953_4400195606972806_8336303907875827476_n.jpg"><div class="caption">Bulldozer Unit 6</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/655171953_4400195606972806_8336303907875827476_n.jpg"><div class="caption">Bulldozer Unit 6</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/659726186_4445571869059311_219560017586572443_n.jpg"><div class="caption">Bulldozer Unit 7</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/659726186_4445571869059311_219560017586572443_n.jpg"><div class="caption">Bulldozer Unit 7</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/653809507_785696160889629_5217017849474593867_n.jpg"><div class="caption">Bulldozer Unit 8</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/653809507_785696160889629_5217017849474593867_n.jpg"><div class="caption">Bulldozer Unit 8</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/655191211_1563714621366146_1141032871636098971_n.jpg"><div class="caption">Bulldozer Unit 9</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/655191211_1563714621366146_1141032871636098971_n.jpg"><div class="caption">Bulldozer Unit 9</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item bulldozer show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Bulldozer/653834543_4412166759072825_8147844889670856743_n.JPG"><div class="caption">Bulldozer Unit 10</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Bulldozer/653834543_4412166759072825_8147844889670856743_n.JPG"><div class="caption">Bulldozer Unit 10</div></div>
                 </div>
 
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/658985344_1634677104089417_2744807182855787149_n.jpg"><div class="caption">Truck Unit 1</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/658985344_1634677104089417_2744807182855787149_n.jpg"><div class="caption">Truck Unit 1</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/461055332_1557776181782226_6506807053802612864_n.jpg"><div class="caption">Truck Unit 2</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/461055332_1557776181782226_6506807053802612864_n.jpg"><div class="caption">Truck Unit 2</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/462576050_2874257402778963_3117141770811188863_n.jpg"><div class="caption">Truck Unit 3</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/462576050_2874257402778963_3117141770811188863_n.jpg"><div class="caption">Truck Unit 3</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/655666709_1084572513880303_7853607595254985515_n.jpg"><div class="caption">Truck Unit 4</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/655666709_1084572513880303_7853607595254985515_n.jpg"><div class="caption">Truck Unit 4</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/653801600_1910754536237602_8560765101893494250_n.jpg"><div class="caption">Truck Unit 5</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/653801600_1910754536237602_8560765101893494250_n.jpg"><div class="caption">Truck Unit 5</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/653765689_1183725430366112_6031211004507062559_n.jpg"><div class="caption">Truck Unit 6</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/653765689_1183725430366112_6031211004507062559_n.jpg"><div class="caption">Truck Unit 6</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/658808181_983877477413077_939188699834384718_n.jpg"><div class="caption">Truck Unit 7</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/658808181_983877477413077_939188699834384718_n.jpg"><div class="caption">Truck Unit 7</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/658985344_1634677104089417_2744807182855787149_n.jpg"><div class="caption">Truck Unit 8</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/658985344_1634677104089417_2744807182855787149_n.jpg"><div class="caption">Truck Unit 8</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/461055332_1557776181782226_6506807053802612864_n.jpg"><div class="caption">Truck Unit 9</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/461055332_1557776181782226_6506807053802612864_n.jpg"><div class="caption">Truck Unit 9</div></div>
                 </div>
                 <div class="col-lg-3 col-md-6 gallery-item trucks show">
-                    <div class="project-card"><img src="IMG/frommessenger_img/Trucks/653741205_1650177919468981_5671678125654794014_n.jpg"><div class="caption">Truck Unit 10</div></div>
+                    <div class="project-card"><img src="img/frommessenger_img/Trucks/653741205_1650177919468981_5671678125654794014_n.jpg"><div class="caption">Truck Unit 10</div></div>
                 </div>
             </div>
         </div>
